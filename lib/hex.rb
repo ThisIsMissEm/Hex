66058059dd8fc7acebfdd3166affb95875327a5d
Micheil Smith <micheil@brandedcode.com>
2010-02-11 23:49:24 +1100

    Okay, so I get the point that textmate doesn't like soft-tabs.

--- lib/hex.rb
+++ lib/hex.rb
@@ -2,33 +2,28 @@ require 'yaml'
 require 'erb'
 require 'rack'
 
-
-
 module Hex
-	
-	class Site
-		
-	end
-	
-	class Server
-		def initialize
-						
-		end
-		
-		def call env
-			@request = Rack::Request.new env
-			@response = Rack::Response.new
-			
-			return [400, {}, []] unless @request.get?
-			
-			path, filetype = @request.path_info.split('.')
-			
-			@response.body = ["path"]
-			@response['Content-Length'] = 4.to_s
-			@response.status = 200
-			@response.finish
-		end
-	end
+  class Site
+  end
+  
+  class Server
+    def initialize
+    end
+    
+    def call env
+      @request = Rack::Request.new env
+      @response = Rack::Response.new
+      
+      return [400, {}, []] unless @request.get?
+      
+      path, filetype = @request.path_info.split('.')
+      
+      @response.body = ["path"]
+      @response['Content-Length'] = 4.to_s
+      @response.status = 200
+      @response.finish
+    end
+  end
 end
 
 
